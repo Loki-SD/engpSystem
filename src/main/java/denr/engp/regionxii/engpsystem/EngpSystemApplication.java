0193373c4448e5af1c19f373636d935a266814f4
Loki-SD <michaelloquias96@gmail.com>
2023-04-10 18:18:45 +0800

    first commit

---
+++ src/main/java/denr/engp/regionxii/engpsystem/EngpSystemApplication.java
@@ -0,0 +1,13 @@
+package denr.engp.regionxii.engpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngpSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/denr/engp/regionxii/engpsystem/EngpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package denr.engp.regionxii.engpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
